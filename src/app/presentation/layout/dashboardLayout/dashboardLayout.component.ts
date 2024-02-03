0e3b1491abd9840fbb3fc87a5eaf1e5b6cb5b063
Dante Arrighi <dantearrighi@gmail.com>
2024-02-03 13:35:44 -0300

    Added Sidebar Menu

--- src/app/presentation/layout/dashboardLayout/dashboardLayout.component.ts
+++ src/app/presentation/layout/dashboardLayout/dashboardLayout.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { SidebarMenuItemComponent } from '../../components/sidebarMenuItem/sidebarMenuItem.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarMenuItemComponent],
   templateUrl: './dashboardLayout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+  public routes = routes[0].children?.filter((route) => route.data);
+}
